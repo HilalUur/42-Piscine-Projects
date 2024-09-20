6cc40e1c3a9347ee07ea2ba5ea2c33cb1dee549d
Hilal <hilarugur@gmail.com>
2024-09-20 15:55:17 +0300

    Update main.c

--- C Projects/C01/ex08/main.c
+++ C Projects/C01/ex08/main.c
@@ -2,7 +2,7 @@ int main()
 {
     int arr[] = {13, 2, 55, 49, 9, 6};
     int size = 6;
-    ft_rev_int_tab(arr, size);
+    ft_sort_int_tab(arr, size);
     printf("%d,%d,%d,%d,%d,%d", arr[0], arr[1], arr[2], arr[3], arr[4], arr[5]);
     return 0;
 }
