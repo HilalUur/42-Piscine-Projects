a0bb979134c9fb7d979d68cf4b7ef84d2d81a26e
Hilal <hilarugur@gmail.com>
2024-09-20 15:53:03 +0300

    Create main.c

---
+++ C Projects/C01/ex08/main.c
@@ -0,0 +1,8 @@
+int main()
+{
+    int arr[] = {13, 2, 55, 49, 9, 6};
+    int size = 6;
+    ft_rev_int_tab(arr, size);
+    printf("%d,%d,%d,%d,%d,%d", arr[0], arr[1], arr[2], arr[3], arr[4], arr[5]);
+    return 0;
+}
